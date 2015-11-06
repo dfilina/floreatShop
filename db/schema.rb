9f4cc7d0c0c9de6e0d2b21fc314386959fa6017d
Dmitry Filina <dmitry.filina@gmail.com>
2015-11-06 12:50:15 -0600

    Added products scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103042205) do
+ActiveRecord::Schema.define(version: 20151106184409) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price"
+    t.boolean  "availability", default: true
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
